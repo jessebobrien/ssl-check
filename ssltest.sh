bf969ff15d4de81d4d6bd6fb75377862b65d31fa
nshobe <nshobe@cironline.org>
2014-04-14 14:01:39 -0700

    Changes made:
    daysleft variable gives a readable format for "datediff" variable in days
    
    checkurl function verifies the URL exists, and should qualify local domains just fine
    
    checkssl verifies there's a working ssl pull so that sites with no ssl info don't come back successful
    
    made a few formatting changes to keep the output neat with said changes

--- ssltest.sh
+++ ssltest.sh
@@ -16,7 +16,31 @@ function getUrl()
 	# puts input into a string
 	read URL
 }
-
+function checkUrl()
+{
+        # gives us a value to work with.
+        checkUrl=`dig +short $URL`
+        #checks output and displays message and gives boolean output to work with
+        if [ -n "$checkUrl" ]; then
+                printf "\nHostname: $URL is valid.\n"
+                urlGood=true
+        else
+                printf "\nCould NOT verify $URL exists!\n"
+                urlGood=false
+        fi
+}
+function checkssl
+{
+        # gives variable string conetent if we found the certificate
+        certexists=$(echo | openssl s_client -connect "$URL":443 2>/dev/null | openssl x509 -noout -dates | grep After)
+        # exits script if no variable value with error message
+        if [[ $certexists == *After* ]]; then
+                printf "\nSSL Appears to be present\n"
+        else
+                printf "\nSSL was not found!\n"
+                exit 0
+        fi
+}
 function pullCert()
 {
 	# pulls entire cert for given URL
@@ -33,19 +57,24 @@ function pullCert()
 	epochnow=`date +%s`
 	# calculate differnce between epoch dates
 	datediff=$(expr "$epochcert" - "$epochnow")
+        # give days left in readable format
+        daysleft=$(($datediff/86400))
 }
 
 function drawReport()
 {
 	# draw results
-	printf "%-20s %-40s %-20s\n" "URL" "Expires" "Difference (epoch)" #header
+	printf "\n%-20s %-40s %-20s\n" "URL" "Expires" "Difference (epoch)" #header
 	printf "%-20s %-40s %-20s\n\n" "${URL}" "${certexp}" "${datediff}" #data
 
 	# verify difference isn't less than 60 days
 	if [ "$datediff" -lt "5184000" ]; then
 			printf "!!!!This cert is expiring on %s!!!!", $certexp
+                        printf "\n\nAn email has been sent to the root user of this machine\n\n"
+                        echo "ATTENTION! This cert is expiring in $daysleft days, please correct immediately: ""$URL" | \
+                         mail -s "SSL Expiration Warning!" root
 	else
-			echo "This cert has more than 60 days left, no rush. Go grab some coffee."
+			echo "This cert has $daysleft days left, no rush. Go grab some coffee."
 	fi
 }
 
@@ -56,9 +85,18 @@ function main()
 	# If the URL isn't already set, request it.
 	if [[ -z "$URL" ]]; then
 		getUrl
+		checkUrl
+		if [ "$urlGood" = false ]; then
+                        exit 0
+                fi
 	else
 		URL="$1"
+                checkUrl
+                if [ "$urlGood" = false ]; then
+                        exit 0
+                fi
 	fi
+	checkssl
 	pullCert
 	drawReport
 }
